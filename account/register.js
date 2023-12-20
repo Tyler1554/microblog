21f59a432e3cc1bcdc72f6da373fd77725f5f29f
J Gdovic <jgdovic3@gmail.com>
2023-12-20 15:56:44 -0500

    variables for input form

---
+++ account/register.js
@@ -0,0 +1,7 @@
+"use strict";
+//variables for register page
+const nameFeild = document.querySelector("#nameFeild");
+const userNameFeild = document.querySelector("#userNameFeild");
+const passwordFeild = document.querySelector("#passwordFeild");
+const confirmPasswordFeild = document.querySelectorAll("#confirmPasswordFeild");
+const inputFeildButton = document.querySelector("#inputFeildButton");

1061e3f11472ceeb604ba86a80360de5b4f755d2
Tyler1554 <tyler1554@yahoo.com>
2023-12-21 14:16:54 -0500

    post page set up

--- posts/posts.js
+++ posts/posts.js
@@ -1,3 +1,24 @@
-/* Posts Page JavaScript */
-
 "use strict";
+
+const postCard = document.querySelector(".post-card");
+const likeButton = document.querySelector("#like-button");
+
+function loadPosts() {
+  const loginData = getLoginData();
+  fetch("http://microbloglite.us-east-2.elasticbeanstalk.com/api/posts", {
+    headers: {
+      Authorization: `Bearer ${loginData.token}`,
+    },
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      for (let post of data) {
+        let userName = document.createElement("h3");
+        userName.innerText = post.username;
+        postCard.appendChild(userName);
+        // postContent.innerText = post.text;
+      }
+    });
+}
+
+loadPosts();

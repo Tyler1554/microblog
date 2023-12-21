322ffc9413c379a567602f7abfe9ec9927c310de
Talib Smith <talibsmith77@gmail.com>
2023-12-21 13:56:55 -0500

    profile javascript

--- profile/profile.js
+++ profile/profile.js
@@ -1,2 +1,8 @@
-"use strict"
+"use strict";
 
+async function get_profile() {
+  const response = await fetch("http://microbloglite.us-east-2.elasticbeanstalk.com/api/users")
+  const data = await response.json();
+  console.log(data);
+}
+get_profile();

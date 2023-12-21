5f9a51ea3a91869abadb6c1245b0dc0b13c8511a
J Gdovic <jgdovic3@gmail.com>
2023-12-21 13:56:07 -0500

    got register page working but not showing proof

--- account/register.js
+++ account/register.js
@@ -1,7 +1,31 @@
 "use strict";
 //variables for register page
-const nameFeild = document.querySelector("#nameFeild");
-const userNameFeild = document.querySelector("#userNameFeild");
-const passwordFeild = document.querySelector("#passwordFeild");
-const confirmPasswordFeild = document.querySelectorAll("#confirmPasswordFeild");
-const inputFeildButton = document.querySelector("#inputFeildButton");
+let nameFeild = document.querySelector("#nameFeild");
+let userNameFeild = document.querySelector("#userNameFeild");
+let passwordFeild = document.querySelector("#passwordFeild");
+let inputFeildButton = document.querySelector("#inputFeildButton");
+
+function createUser() {
+  let newNameFeild = nameFeild.value;
+  let newUserNameFeild = userNameFeild.value;
+  let newPasswordFeild = passwordFeild.value;
+
+  const createdUser = {
+    username: `${newUserNameFeild}`,
+    fullName: `${newNameFeild}`,
+    password: `${newPasswordFeild}`,
+  };
+
+  fetch("http://microbloglite.us-east-2.elasticbeanstalk.com/api/users", {
+    method: "POST",
+    headers: { "content-Type": "application/json" },
+    body: JSON.stringify(createdUser),
+  })
+    .then((resopnse) => resopnse.json())
+    .then((createdUser) => {
+      window.location.href = "login.html";
+      return createdUser;
+    });
+}
+
+inputFeildButton.onclick = createUser;

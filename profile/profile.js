2bd3a4a2574e3aa0db668fec868ae8046e7ccc7d
Talib Smith <talibsmith77@gmail.com>
2024-01-01 19:27:18 -0500

    using token

--- profile/profile.js
+++ profile/profile.js
@@ -1,29 +1,33 @@
 "use strict";
 let users_list = document.querySelector("#users_list");
 
-async function get_users() {
-  const response = await fetch("http://microbloglite.us-east-2.elasticbeanstalk.com/api/users/", {
-    headers: {
-      "content-Type": "application/json",
-      Authorization:
-        "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6InlhbXVtMTExIiwiaWF0IjoxNzAzODc3MzQwLCJleHAiOjE3MDM5NjM3NDB9.2LHsy07ezsYJHA9Pq8t4ahLcUW5eMmb0tFy5lFMojMs",
-    },
-  });
-  const users = await response.json();
-  for (const user of users) {
-    let option = document.createElement("option");
-    option.innerText = user.username;
-    option.value = user.username;
-    users_list.appendChild(option);
-  }
-}
+
+
+
+// async function get_users() {
+  
+//   const response = await fetch("http://microbloglite.us-east-2.elasticbeanstalk.com/api/users/", {
+//     headers: {
+//       "content-Type": "application/json",
+//       Authorization:
+//         "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6InlhbXVtMTExIiwiaWF0IjoxNzA0MTU0MDIyLCJleHAiOjE3MDQyNDA0MjJ9.t03n-mULgLuxTRdY4SkgAkATCqVHhEVI1H7Am3FQ7L4",
+//     },
+//   });
+//   const users = await response.json();
+//   for (const user of users) {
+//     let option = document.createElement("option");
+//     option.innerText = user.username;
+//     option.value = user.username;
+//     users_list.appendChild(option);
+//   }
+// }
 async function get_user() {
-  let user_name = users_list.value;
-  const response = await fetch(`http://microbloglite.us-east-2.elasticbeanstalk.com/api/users/${user_name}`, {
+  let log_in_data = getloginData();
+  const response = await fetch(`http://microbloglite.us-east-2.elasticbeanstalk.com/api/users/${log_in_data.username}`, {
     headers: {
       "content-Type": "application/json",
       Authorization:
-        "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6InlhbXVtMTExIiwiaWF0IjoxNzAzODc3MzQwLCJleHAiOjE3MDM5NjM3NDB9.2LHsy07ezsYJHA9Pq8t4ahLcUW5eMmb0tFy5lFMojMs",
+        `Bearer ${log_in_data.token}`,
     },
   });
   const user = await response.json();
@@ -48,14 +52,26 @@ async function get_post() {
     headers: {
       "content-Type": "application/json",
       Authorization:
-        "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6InlhbXVtMTExIiwiaWF0IjoxNzAzODc3MzQwLCJleHAiOjE3MDM5NjM3NDB9.2LHsy07ezsYJHA9Pq8t4ahLcUW5eMmb0tFy5lFMojMs",
+        "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6InlhbXVtMTExIiwiaWF0IjoxNzA0MTU0MDIyLCJleHAiOjE3MDQyNDA0MjJ9.t03n-mULgLuxTRdY4SkgAkATCqVHhEVI1H7Am3FQ7L4",
     },
   });
   const posts = await response.json()
   for (const post of posts) {
-    console.log(post);
+    let post_div = document.createElement("div")
+    let post_text = document.createElement("p")
+    let time_stamp = document.createElement("p")
+
+    post_text.textContent = post.text
+    time_stamp.textContent = post.createdAt
+
+    post_div.appendChild(post_text)
+    post_div.appendChild(time_stamp)
+    document.body.appendChild(post_div)
+
+    console.log(post)
+
   }
 }
 
-users_list.onchange = get_user;
-window.onload = get_users;
+ get_user();
+// window.onload = get_users;

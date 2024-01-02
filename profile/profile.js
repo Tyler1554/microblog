06b4fd2535aea8c693ca09faa36cc8f4f230e9a0
Talib Smith <talibsmith77@gmail.com>
2024-01-02 09:53:14 -0500

    profile page working and styling

--- profile/profile.js
+++ profile/profile.js
@@ -1,35 +1,20 @@
 "use strict";
 let users_list = document.querySelector("#users_list");
+let main_container = document.querySelector("#main_container");
+let background_container = document.querySelector("#background_container");
+let bio_container = document.querySelector("#bio_container");
+let log_in_data = getLoginData();
 
-
-
-
-// async function get_users() {
-  
-//   const response = await fetch("http://microbloglite.us-east-2.elasticbeanstalk.com/api/users/", {
-//     headers: {
-//       "content-Type": "application/json",
-//       Authorization:
-//         "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6InlhbXVtMTExIiwiaWF0IjoxNzA0MTU0MDIyLCJleHAiOjE3MDQyNDA0MjJ9.t03n-mULgLuxTRdY4SkgAkATCqVHhEVI1H7Am3FQ7L4",
-//     },
-//   });
-//   const users = await response.json();
-//   for (const user of users) {
-//     let option = document.createElement("option");
-//     option.innerText = user.username;
-//     option.value = user.username;
-//     users_list.appendChild(option);
-//   }
-// }
 async function get_user() {
-  let log_in_data = getloginData();
-  const response = await fetch(`http://microbloglite.us-east-2.elasticbeanstalk.com/api/users/${log_in_data.username}`, {
-    headers: {
-      "content-Type": "application/json",
-      Authorization:
-        `Bearer ${log_in_data.token}`,
-    },
-  });
+  const response = await fetch(
+    `http://microbloglite.us-east-2.elasticbeanstalk.com/api/users/${log_in_data.username}`,
+    {
+      headers: {
+        "content-Type": "application/json",
+        Authorization: `Bearer ${log_in_data.token}`,
+      },
+    }
+  );
   const user = await response.json();
 
   let profile_div = document.createElement("div");
@@ -38,36 +23,37 @@ async function get_user() {
 
   bio.textContent = user.bio;
   profile_name.textContent = user.username;
+  profile_div.appendChild(profile_name);
+  bio_container.appendChild(bio);
+  background_container.appendChild(profile_div);
   console.log(user);
-  get_post()
+  get_post();
 }
 
 async function get_post() {
-  let user_post = users_list.value;
+  let user_post = log_in_data.username;
   const response = await fetch(`http://microbloglite.us-east-2.elasticbeanstalk.com/api/posts?username=${user_post}`, {
     headers: {
       "content-Type": "application/json",
-      Authorization:
-        "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6InlhbXVtMTExIiwiaWF0IjoxNzA0MTU0MDIyLCJleHAiOjE3MDQyNDA0MjJ9.t03n-mULgLuxTRdY4SkgAkATCqVHhEVI1H7Am3FQ7L4",
+      Authorization: `Bearer ${log_in_data.token}`,
     },
   });
-  const posts = await response.json()
+  const posts = await response.json();
   for (const post of posts) {
-    let post_div = document.createElement("div")
-    let post_text = document.createElement("p")
-    let time_stamp = document.createElement("p")
+    let post_div = document.createElement("div");
+    let post_text = document.createElement("p");
+    let time_stamp = document.createElement("p");
 
-    post_text.textContent = post.text
-    time_stamp.textContent = post.createdAt
+    post_text.textContent = post.text;
+    time_stamp.textContent = post.createdAt;
 
-    post_div.appendChild(post_text)
-    post_div.appendChild(time_stamp)
-    document.body.appendChild(post_div)
+    post_div.appendChild(post_text);
+    post_div.appendChild(time_stamp);
+    document.body.appendChild(post_div);
 
-    console.log(post)
-
+    console.log(post);
   }
 }
 
- get_user();
+get_user();
 // window.onload = get_users;
